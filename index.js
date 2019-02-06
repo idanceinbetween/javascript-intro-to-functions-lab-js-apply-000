5f005987297714d7859031eaa17f466a4d575b50
J Hon <jx@blackwingedcreatives.com>
2019-02-06 13:22:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,16 +15,12 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  if string.toLowerCase() === string;
-  return `I can't hear you!`
-}
-
-function sayHiToGrandma(string){
-  if string.toUpperCase() === string;
-  return `YES INDEED!`
-}
-
-function sayHiToGrandma(string){
-  if string.toLowerCase() === string;
-  return `I love you, too.`
+  if (string.toLowerCase() === string){
+      return `I can't hear you!`
+  }
+  elseif(string.toUpperCase() === string){
+      return `YES INDEED!`
+  } else {
+    return `I love you, too.`
+  }
 }
