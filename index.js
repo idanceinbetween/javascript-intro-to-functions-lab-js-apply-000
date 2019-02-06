02742a7b7d35ce6427abdbc87be4fee2f7f42f88
J Hon <jx@blackwingedcreatives.com>
2019-02-06 13:23:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function sayHiToGrandma(string){
   if (string.toLowerCase() === string){
       return `I can't hear you!`
   }
-  elseif(string.toUpperCase() === string){
+  else if(string.toUpperCase() === string){
       return `YES INDEED!`
   } else {
     return `I love you, too.`
